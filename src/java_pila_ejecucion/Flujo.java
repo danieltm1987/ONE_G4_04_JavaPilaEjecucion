0b0f95d112699b3e3c73fe653950fd1e69f7eed2
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-24 22:16:28 -0500

    commit 03: uso del try / catch

--- src/java_pila_ejecucion/Flujo.java
+++ src/java_pila_ejecucion/Flujo.java
@@ -6,22 +6,35 @@ public class Flujo {
 		System.out.println("Inicio main !!!");
 		metodo1();
 		System.out.println("Fin main");
-
 	}
 
 	private static void metodo1() {
 		System.out.println("Inicio metodo 1!!!");
 		metodo2();
 		System.out.println("Fin metodo 1");
-		
 	}
 
 	private static void metodo2() {
 		System.out.println("Inicio metodo 1!!!");
 		for (int i = 1; i <=5; i++) {
-			System.out.println(i);			
+			System.out.println(i);
+			
+			try { // java intenta esto...
+				
+				if (i == 3) {
+					int num = 0;
+					int resultado = i/num;
+				}
+				
+				String test = null;
+				System.out.println(test.toString());
+				
+			}catch (ArithmeticException| NullPointerException exception) {
+				System.out.println("Atrapo Excepcion");
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+			}
 		}
 		System.out.println("Fin metodo 1");
-		
 	}
 }
