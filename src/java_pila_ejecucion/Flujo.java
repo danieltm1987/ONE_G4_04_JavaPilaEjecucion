0c72d70961ab7dd2647e6e4963821f3f66bd8e19
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-24 21:49:31 -0500

    commit 02: se crea un punto de interrupcion y se ejecuta el modo debug.

--- src/java_pila_ejecucion/Flujo.java
+++ src/java_pila_ejecucion/Flujo.java
@@ -3,8 +3,25 @@ package java_pila_ejecucion;
 public class Flujo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola excepciones");
+		System.out.println("Inicio main !!!");
+		metodo1();
+		System.out.println("Fin main");
 
 	}
 
+	private static void metodo1() {
+		System.out.println("Inicio metodo 1!!!");
+		metodo2();
+		System.out.println("Fin metodo 1");
+		
+	}
+
+	private static void metodo2() {
+		System.out.println("Inicio metodo 1!!!");
+		for (int i = 1; i <=5; i++) {
+			System.out.println(i);			
+		}
+		System.out.println("Fin metodo 1");
+		
+	}
 }
