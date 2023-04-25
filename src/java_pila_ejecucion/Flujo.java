6f92a8bedb6bc0de5593fcfcddbb9a07316cd3e8
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-24 21:43:48 -0500

    commit 01: funcionamiento de la pila de ejecucion en JAVA JVM.

---
+++ src/java_pila_ejecucion/Flujo.java
@@ -0,0 +1,10 @@
+package java_pila_ejecucion;
+
+public class Flujo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola excepciones");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author danieltm1987
+ *
+ */
+module java_pila_ejecucion {
+}
